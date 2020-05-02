32bc60465e9a7ff7112337b6bb894b4dd2b4a57d
Lebedev Konstantin <ibnrubaxa@gmail.com>
2020-05-02 14:22:01 +0300

    + гуппы

--- draft_config.go
+++ draft_config.go
@@ -5,6 +5,10 @@ import "github.com/gothing/draft"
 func init() {
 	// Инициализируем GODRAFT :: DOC
 	draft.SetupDoc(draft.DocConfig{
-		FrontURL: "https://gothing.github.io/draft-front/",
+		FrontURL:    "https://gothing.github.io/draft-front/",
+		ActiveGroup: "demo",
+		Groups: []draft.DocGroup{
+			{"demo", "Demo", []string{"http://localhost:2047/godraft:scheme/"}},
+		},
 	})
 }

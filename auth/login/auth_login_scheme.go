1db536fab3ae884ab959fb6a21b395ed4aec220f
Lebedev Konstantin <ibnrubaxa@gmail.com>
2020-05-02 13:26:46 +0300

    + добавляем описание параметров

--- auth/login/auth_login_scheme.go
+++ auth/login/auth_login_scheme.go
@@ -2,9 +2,17 @@ package login
 
 import (
 	"github.com/gothing/draft"
+	"github.com/gothing/draft/types"
 )
 
 // AuthLogin — сткрутура описания endpoint
 type AuthLogin struct {
 	draft.Endpoint
+}
+
+// AuthLoginParams — параметры запроса
+type AuthLoginParams struct {
+	Login    types.Login    `required:"true"`
+	Password types.Password `required:"true"`
+	Remember bool           `comment:"Запомнить сессию"` // инлайн комментарий
 }
